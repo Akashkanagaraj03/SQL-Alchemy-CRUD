568333f65e1ab2046176c9246b8992e887e37c91
akash <akashkanagaraj03@gmail.com>
2025-02-19 04:41:28 +0530

    added delete and other changes

--- delete.py
+++ delete.py
@@ -6,8 +6,7 @@ session = Session(bind = engine)
 
 update_item = session.query(Review).filter_by(user_id = 4, movie_id = 6).first()
 
-update_item.review_txt = "Edited movie review"
-
+session.delete(update_item)
 session.commit()
 session.close()
 
--- fetch.py
+++ fetch.py
@@ -11,17 +11,17 @@ print("\nUsers in the table:")
 for user in users:
     print(f'{user.id}. {user.name}')
 
-#fetching reviews of a certain movie
+#fetching reviews of a certain movie -> result_1
 # statement_1 = select(Review).join(Movie).where(Movie.id == 6)                #does not use orm
 # result_1 = session.scalars(statement_1)
 result_1 = session.query(Review).join(Movie).filter(Movie.id == 6)             #uses orm
 
-#fetching all movies reviews by a certain user
+#fetching all movies reviews by a certain user -> result_2
 # statement_2 = select(Review.movie_id).join(User).where(User.id == 4)         #does not use orm
 # statement_3 = select(Movie).where(Movie.id.in_(statement_2))
 #result_2 = session.scalars(statement_3)
-statement_2 = session.query(Review.movie_id).join(User).filter(User.id == 4)   #uses orm
-result_2 = session.query(Movie).filter(Movie.id.in_(statement_2)).all()
+sub_query = session.query(Review.movie_id).join(User).filter(User.id == 4)   #uses orm
+result_2 = session.query(Movie).filter(Movie.id.in_(sub_query)).all()
 
 print("\nReviews of The Lord of the Rings:")
 for review in result_1:
